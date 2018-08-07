e6c9fb611517585b1d021201e209004e8a073544
Martynas <mm1544@york.ac.uk>
2018-08-07 12:22:21 +0100

    Retrieve locally saved data

--- app/src/main/java/com/martyThePunisher/flashchatnewfirebase/MainChatActivity.java
+++ app/src/main/java/com/martyThePunisher/flashchatnewfirebase/MainChatActivity.java
@@ -1,5 +1,6 @@
 package com.martyThePunisher.flashchatnewfirebase;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
@@ -21,6 +22,7 @@ public class MainChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_chat);
 
         // TODO: Set up the display name and get the Firebase reference
+        setupDisplayName();
 
 
         // Link the Views in the layout to the Java code
@@ -36,7 +38,27 @@ public class MainChatActivity extends AppCompatActivity {
     }
 
     // TODO: Retrieve the display name from the Shared Preferences
+    private void setupDisplayName(){
 
+        SharedPreferences prefs = getSharedPreferences(RegisterActivity.CHAT_PREFS, MODE_PRIVATE);
+        // FOR THE NAME WE WILL supply a stati# #onstant in the RegisterA#tivity #alled CHAT_PREFS.
+        // And for the mode we will use PRIVATE
+
+        // NOTE: we #an multiple shared preferen#es inside the same app, SO we will give to ea#h
+        // shared pref. its own name.
+
+        // retrieving the data:
+            // To extra#t saved data from sharedPreferen#es we will be using getString() method.
+        // getString() requires the key, the one that we have saved. We will provide the KEY in the
+        // form of a stati# DISPLAY_NAME_KEY #onstant from RegisterA#tivity:
+        mDisplayName = prefs.getString(RegisterActivity.DISPLAY_NAME_KEY, null);
+
+        // if there is no Display Name that has been saved under the Shared Preferen#es
+        if(mDisplayName == null) mDisplayName = "Anonymous";
+
+        // now we have to #all setupDisplayName() method inside onCreate in MainChatActivity
+
+    }
 
     private void sendMessage() {
 
